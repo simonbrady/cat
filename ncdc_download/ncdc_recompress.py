2a142a378d4a83d2dd3ad9710f714b197af911a6
Simon Brady <simon@hikari.org.nz>
2015-06-18 11:17:39 +1200

    ncdc_download: parallelise ncdc_recompress.py

--- ncdc_download/ncdc_recompress.py
+++ ncdc_download/ncdc_recompress.py
@@ -7,6 +7,7 @@ import os
 import stat
 from subprocess import PIPE, Popen
 import sys
+import time
 
 # Command line arguments
 args = None
@@ -17,15 +18,36 @@ region_map = {}
 # Input files for each region encountered
 input_files = {}
 
+# Active compression worker tasks, stored as tuples of (gzip process, bzip2
+# process, output filename, output file)
+workers = []
+
+# Range-checked integer command line arguments, inspired by
+# http://stackoverflow.com/questions/12116685/
+def positive_int(arg):
+    val = int(arg)
+    if val > 0:
+        return val
+    else:
+        raise argparse.ArgumentTypeError("{0} is not a positive integer".
+                                         format(arg))
+
 # Parse command line and set up args global variable with result
 def parse_args():
     global args
     parser = argparse.ArgumentParser()
+    output_opts = parser.add_mutually_exclusive_group()
+    output_opts.add_argument("-q", "--quiet", action="store_true",
+                             help="suppress normal output")
+    output_opts.add_argument("-v", "--verbose", action="store_true",
+                             help="verbose output")
+    parser.add_argument("-p", "--poll-interval", type=positive_int, default=10,
+                        help="seconds between polling workers (default 10)")
     parser.add_argument("-r", "--region-map", type=str,
                         default="station_regions.txt", help="file of station" +
                             "-to-region mappings, default station_regions.txt")
-    parser.add_argument("-v", "--verbose", action="store_true",
-                        help="verbose output")
+    parser.add_argument("-w", "--workers", type=positive_int, default=1,
+                        help="number of compression worker tasks (default 1)")
     parser.add_argument("source_dir", type=str,
                         help="directory of NCDC files, one subdir per year")
     parser.add_argument("output_pattern", type=str,
@@ -37,7 +59,7 @@ def parse_args():
     args = parser.parse_args()
 
 def log(msg):
-    if args.verbose:
+    if not args.quiet:
         print datetime.now().ctime(), msg
 
 def fatal(msg):
@@ -70,46 +92,69 @@ def scan_input(year):
             region = region_map.get(station, "E")
             size = os.stat(filename)[stat.ST_SIZE]
             if input_files.get(region) == None:
-                input_files[region] = [0, []]
-            input_files[region][0] += size
-            input_files[region][1].append(filename)
+                input_files[region] = [[], 0]
+            input_files[region][0].append(filename)
+            input_files[region][1] += size
     except Exception as e:
         fatal("Error processing {0}: {1}".format(year, str(e)))
 
-def create_output_files():
-    work_list = sorted(input_files.items(), key=lambda x: x[1][0])
-    while len(work_list) > 0:
-        item = work_list.pop()
-        output_filename = args.output_pattern.format(item[0])
-        log("Creating {0} from {1} input files ({2} bytes)".
-            format(output_filename, len(item[1][1]), item[1][0]))
-        output_file = None
-        try:
-            output_file = open(output_filename, "w")
-            gzip = Popen(["gzip", "-cd"] + item[1][1], stdout=PIPE,
-                         stderr=PIPE, bufsize=-1)
-            bzip2 = Popen(["bzip2", "-c", "-9"], stdin=gzip.stdout,
-                          stdout=output_file, stderr=PIPE)
-            log("gzip returned {0}, bzip2 returned {1}".
-                format(Popen.wait(gzip), Popen.wait(bzip2)))
+def add_worker(region, input_files, size):
+    global workers
+    output_filename = args.output_pattern.format(region)
+    log("Creating {0} from {1} input files ({2} bytes)".
+        format(output_filename, len(input_files), size))
+    try:
+        output_file = open(output_filename, "w")
+        gzip = Popen(["gzip", "-cd"] + input_files, stdout=PIPE,
+                     stderr=PIPE, bufsize=-1)
+        bzip2 = Popen(["bzip2", "-c", "-9"], stdin=gzip.stdout,
+                      stdout=output_file, stderr=PIPE)
+        workers.append((gzip, bzip2, output_filename, output_file))
+    except Exception as e:
+        fatal("Error creating {0}: {1}".format(output_filename, str(e)))
+
+def wait_for_worker():
+    global workers
+    completed = False
+    while not completed:
+        if args.verbose:
+            log("Waiting for worker task to complete ({0} active)".
+                format(len(workers)))
+        time.sleep(args.poll_interval)
+        for worker in workers:
+            gzip, bzip2, output_filename, output_file = worker
+            if Popen.poll(gzip) == None and Popen.poll(bzip2) == None:
+                continue
+            log("Created {0}: gzip returned {1}, bzip2 returned {2}".
+                format(output_filename, Popen.wait(gzip), Popen.wait(bzip2)))
             if gzip.returncode != 0:
                 sys.stdout.write(Popen.communicate(gzip)[1])
             if bzip2.returncode != 0:
                 sys.stdout.write(Popen.communicate(bzip2)[1])
             output_file.close()
-        except Exception as e:
-            log("Error writing {0}: {1}".format(output_filename, str(e)))
-        finally:
-            if output_file:
-                output_file.close()
+            workers.remove(worker)
+            completed = True
+
+def create_output_files():
+    work_list = sorted(input_files.items(), key=lambda x: x[1][1])
+    while len(work_list) > 0:
+        # Wait for a free worker task slot
+        while len(workers) == args.workers:
+            wait_for_worker()
+        item = work_list.pop()
+        add_worker(item[0], item[1][0], item[1][1])
+    # No more work to add, so wait for remaining workers to finish
+    while len(workers) > 0:
+        wait_for_worker()
 
 def main():
+    start_time = datetime.now()
     parse_args()
     load_region_map()
     for year in range(args.start_year, args.end_year + 1):
         scan_input(year)
     create_output_files()
-    log("Done")
+    log("Done (elapsed time {0})".format(str(datetime.now() - start_time)))
 
 if __name__ == '__main__':
     main()
