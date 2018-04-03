a8fb47e86a03fe4762f8ccd253ed52862246db45
Simon Brady <simon@hikari.org.nz>
2018-04-03 16:14:29 +1200

    Log additional download status to stderr

--- ncdc_download/download_mapper2.py
+++ ncdc_download/download_mapper2.py
@@ -9,18 +9,22 @@ host = 'ftp.ncdc.noaa.gov'
 base = '/pub/data/noaa'
 retries = 3
 
+def status(msg):
+    sys.stderr.write('%s\n' % msg)
+    sys.stderr.write('reporter:status:%s\n' % msg)
+
 ftp = ftplib.FTP(host)
 ftp.login()
 for line in sys.stdin:
     (year, filename) = line.strip().split()
     for i in range(retries):
-        sys.stderr.write('reporter:status:Processing file %s/%s (FTP attempt %d of %d)\n' % (year,
-            filename, i + 1, retries))
+        status('Downloading file %s/%s (FTP attempt %d of %d)' % (year, filename, i + 1, retries))
         try:
             ftp.retrbinary('RETR %s/%s/%s' % (base, year, filename), open(filename, 'wb').write)
         except ftplib.all_errors as error:
             sys.stderr.write('%s\n' % error)
             continue
+        status('Decompressing file %s/%s' % (year, filename))
         count = 0
         for record in gzip.open(filename, 'rb'):
             print('%s\t%s' % (year, record.decode('ISO-8859-1').strip()))
