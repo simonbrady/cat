da2e8c1c620918d47ff6166735bf0abb6c4d3efb
Simon Brady <simon@hikari.org.nz>
2018-04-15 12:34:25 +1200

    ncdc_count: Allow multiple input paths

--- ncdc_count/src/main/java/nz/org/hikari/cat/ncdc_count/NcdcRecordCount.java
+++ ncdc_count/src/main/java/nz/org/hikari/cat/ncdc_count/NcdcRecordCount.java
@@ -13,9 +13,9 @@ import org.apache.hadoop.util.ToolRunner;
 
 // Driver for NCDC record count MapReduce job
 //
-// Args: <input location> <output location> [number of reduce tasks]
+// Args: <input location> <output location>
 //
-//       e.g. s3://BUCKETNAME/ncdc/data/ /out 1
+//       e.g. s3://BUCKETNAME/ncdc/data/ /out
 //
 //       (note that /out will be created in HDFS, not the local filesystem,
 //       and that it must not exist prior to running the job).
@@ -34,18 +34,19 @@ public class NcdcRecordCount extends Configured implements Tool {
 	public int run(String[] args) throws Exception {
 		Job job = Job.getInstance(getConf(), "NCDC Record Count");
 		job.setJarByClass(NcdcRecordCount.class);
-		
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
 		job.setMapperClass(NcdcRecordCountMapper.class);
 		job.setCombinerClass(LongSumReducer.class);
 		job.setReducerClass(LongSumReducer.class);
-		if (args.length == 3)
-			job.setNumReduceTasks(Integer.parseInt(args[2]));
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(LongWritable.class);
-		
+
+		if (args.length < 2)
+			throw new IllegalArgumentException("Insufficient command-line arguments");
+		FileInputFormat.setInputDirRecursive(job, true);
+		for (int i = 0; i < args.length - 1; i++)
+			FileInputFormat.addInputPath(job, new Path(args[i]));
+		FileOutputFormat.setOutputPath(job, new Path(args[args.length - 1]));
+
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 
