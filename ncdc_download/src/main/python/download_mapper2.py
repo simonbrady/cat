cdf929d895df95ae8ad08eea250246cc33900be8
Simon Brady <simon@hikari.org.nz>
2020-04-20 10:55:21 +1200

    Improve download mapper error handling

--- ncdc_download/src/main/python/download_mapper2.py
+++ ncdc_download/src/main/python/download_mapper2.py
@@ -3,6 +3,7 @@
 import ftplib
 import gzip
 import os
+import time
 import sys
 from datetime import datetime
 
@@ -22,6 +23,10 @@ def status(msg):
 def fail(year, filename):
     status('Failed to download %s/%s' % (year, filename))
     sys.stderr.write('reporter:counter:NCDC Download Failures,%s,1\n' % year)
+    try:
+        os.remove(filename)
+    except FileNotFoundError:
+        pass # File might not even have been created
 
 ftp = connect(host)
 for line in sys.stdin:
@@ -33,23 +38,26 @@ for line in sys.stdin:
         except ftplib.all_errors as error:
             sys.stderr.write('%s\n' % error)
             if str(error).startswith('421'):
-                sys.stderr.write('Attempting reconnection after idle timeout\n')
+                status('Attempting reconnection after idle timeout')
                 try:
                     ftp = connect(host)
-                    sys.stderr.write('Reconnection succeeded\n')
+                    status('Reconnection succeeded')
                 except ftplib.all_errors as error:
                     sys.stderr.write('%s\n' % error)
                     fail(year, filename)
-                    break
-            continue
-        status('Decompressing file %s/%s' % (year, filename))
-        count = 0
-        for record in gzip.open(filename, 'rb'):
-            print('%s\t%s' % (year, record.decode('ISO-8859-1').strip()))
-            count += 1
-        os.remove(filename)
-        sys.stderr.write('reporter:counter:NCDC Download Records,%s,%d\n' % (year, count))
-        break
+                    sys.exit(1) # Fail the map task if we can't even reconnect
+            else:
+                time.sleep(30) # Unknown error, so delay in case it's something transient
+            continue # Retry download
+        break # Download succeeded
     else:
-        fail(year, filename)
+        fail(year, filename) # Retries exhausted, move on to next file
+        continue
+    status('Decompressing file %s/%s' % (year, filename))
+    count = 0
+    for record in gzip.open(filename, 'rb'):
+        print('%s\t%s' % (year, record.decode('ISO-8859-1').strip()))
+        count += 1
+    os.remove(filename)
+    sys.stderr.write('reporter:counter:NCDC Download Records,%s,%d\n' % (year, count))
 ftp.quit()
