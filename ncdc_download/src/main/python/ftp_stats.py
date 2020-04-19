080d956ae510ea032d3b36680e98fdcd1c75c9c8
Simon Brady <simon@hikari.org.nz>
2020-04-19 14:31:39 +1200

    Tweak FTP stats script

--- ncdc_download/src/main/python/ftp_stats.py
+++ ncdc_download/src/main/python/ftp_stats.py
@@ -13,11 +13,10 @@ for line in sys.stdin:
     year = line.strip()
     sys.stderr.write('%s\r' % year)
     count = max_size = total_size = 0
-    files = ftp.mlsd('%s/%s' % (base, year), ['size'])
-    for f in files:
-        if not f[0].startswith('.'):
+    for (name, facts) in ftp.mlsd('%s/%s' % (base, year), ['size']):
+        if not name.startswith('.'):
             count += 1
-            size = int(f[1].get('size', 0))
+            size = int(facts.get('size', 0))
             if size > max_size:
                 max_size = size
             total_size += size
