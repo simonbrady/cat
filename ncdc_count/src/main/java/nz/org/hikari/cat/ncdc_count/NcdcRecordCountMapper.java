01140deea21cda2b7adfcbeea393780ebc402bb5
Simon Brady <simon@hikari.org.nz>
2020-10-05 18:34:34 +1300

    ncdc_count: Create static LongWritable(1) outside mapper

--- ncdc_count/src/main/java/nz/org/hikari/cat/ncdc_count/NcdcRecordCountMapper.java
+++ ncdc_count/src/main/java/nz/org/hikari/cat/ncdc_count/NcdcRecordCountMapper.java
@@ -12,12 +12,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class NcdcRecordCountMapper
 		extends Mapper<LongWritable, Text, Text, LongWritable> {
 	
+	static final LongWritable one = new LongWritable(1);
+
 	@Override
 	public void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
 		String line = value.toString();
 		String id = line.substring(4, 10) + "-" + line.substring(10, 15);
-		context.write(new Text(id), new LongWritable(1L));
+		context.write(new Text(id), one);
 	}
 
 }
